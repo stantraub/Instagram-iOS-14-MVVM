9d4760c9737e48ca5007515c0c40f3b05c8be48e
Stanley <stanley.traub@gmail.com>
2020-10-23 17:15:10 -0700

    Initial Commit

---
+++ Instagram-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram-MVVM
+//
+//  Created by Stanley Traub on 10/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
